851b55dfc5d36aed505425fb13f600b6660c9636
richardR144 <richard.rigollet@lapiscine.pro>
2025-06-08 13:43:39 +0200

    création entité Article, crud article Admin, read pour utilisateurs, cablé sur list-pièces

--- src/Controller/Admin/AdminArticleController.php
+++ src/Controller/Admin/AdminArticleController.php
@@ -35,7 +35,7 @@ class AdminArticleController extends AbstractController {
            
            $this->addFlash('success', 'Article created successfully!');
            return $this->redirectToRoute('admin-list-articles');
-       }
+        }
             return $this->render('admin/article/create-article.html.twig');
         }
 
@@ -46,6 +46,41 @@ class AdminArticleController extends AbstractController {
             return $this->render('admin/article/list-articles.html.twig', [
                 'articles' => $articles,
         ]);
+    }
+    #[Route('/admin/article/{id}/update', name: 'admin-update-article')]
+public function updateArticle(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+{
+    if ($request->isMethod('POST')) {
+        $article->setTitle($request->request->get('title'));
+        $article->setContent($request->request->get('content'));
+
+        $imageFile = $request->files->get('image');
+        if ($imageFile) {
+            $imageFileName = uniqid() . '.' . $imageFile->guessExtension();
+            $imageFile->move($this->getParameter('article_images_directory'), $imageFileName);
+            $article->setImage($imageFileName);
+        }
+
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Article mis à jour avec succès !');
+        return $this->redirectToRoute('admin-list-articles');
+    }
+
+    return $this->render('admin/article/update-article.html.twig', [
+        'article' => $article,
+    ]);
+}
+
+    #[Route('/admin/article/{id}/delete', name: 'admin-delete-article', methods: ['POST'])]
+    public function deleteArticle(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete_article_' . $article->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+            $this->addFlash('success', 'Article supprimé avec succès !');
+        }
+        return $this->redirectToRoute('admin-list-articles');
     }
 }
 
